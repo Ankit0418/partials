264d78e96a029ec3f85cd3a22e9e5b8885a43686
Ankit0418 <138276382+Ankit0418@users.noreply.github.com>
2024-03-27 14:50:21 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const publicDirectoryPath = path.join(__dirname,'../public');
+const veiwPath = path.join(__dirname,'../templates/views');
+const partialPath = path.join(__dirname,'../templates/partials');
+
+app.set('view engine','hbs')
+app.set('views',veiwPath)
+hbs.registerPartials(partialPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('',(req,res)=>{
+  res.render('index',{
+    title:'ExpressApp',
+    name:'MyName'
+  })
+})
+
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    title:'About me',
+    name:'weather Predector'
+  })
+})
+
+app.get('/help',(req,res)=>{
+  res.render('help',{
+   helptext:'This is some helpful text',
+   title:'Help',
+   name:'MyName'
+  })
+})
+
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000.');
+})
+
+app.get('/ExpressApp', (req, res)=>{
+  res.send({
+    forecast:'It is snowing',
+    location: 'Philadelphia'
+  })
+})
+
+app.get('/help/*',(req,res)=>{
+  res.render('404',{
+    title:'404',
+    name:'Myname',
+    errorMessage:'Help article not found.'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'MyName',
+    errorMessage: 'Page not found'
+  });
+});
